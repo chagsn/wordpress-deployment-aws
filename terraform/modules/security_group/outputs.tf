cb2b90b3eba6363205720cabbc5fd568e1727e64
Charlotte <chagsn@LAPTOP-CABIA5LL>
2024-03-21 17:14:01 +0100

    update modules/security_group/outputs.tf: add database security group id in outputs

--- terraform/modules/security_group/outputs.tf
+++ terraform/modules/security_group/outputs.tf
@@ -1,11 +1,15 @@
+output "db_security_group_id" {
+  value = aws_security_group.db_security_group.id
+}
+
 output "alb_security_group_id" {
-  value = aws_security_group.alb_security_group[*].id
+  value = aws_security_group.alb_security_group.id
 }
 
 output "ecs_security_group_id" {
-   value = aws_security_group.ecs_security_group[*].id
+   value = aws_security_group.ecs_security_group.id
 }
 
 output "efs_security_group_id" {
-   value = aws_security_group.efs_security_group[*].id
+   value = aws_security_group.efs_security_group.id
 }
