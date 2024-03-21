ea6c07c72598dc4abeba7308baa1720770bdff4e
Charlotte <chagsn@LAPTOP-CABIA5LL>
2024-03-21 17:15:06 +0100

    correct reference to database sg in rds module call

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,14 +6,14 @@ module "networking" {
 
 module "security_group" {
   source         = "./modules/security_group"
+  env    = var.env
   vpc_id         = module.networking.vpc_id
-  vpc_cidr_block = module.networking.vpc_cidr_block
 }
 
 module "rds" {
   source            = "./modules/database_rds"
   vpc_db_group      = module.networking.database_subnet_group
-  security_group_id = module.security_group.security_group_id
+  security_group_id = module.security_group.db_security_group_id
   encryption_key    = var.encryption_key
 }
 
