4c5ca608ecf7bd999b5aeb53acfc2e599942b4cb
sergemad <sergekeita01@gmail.com>
2024-03-23 23:10:52 +0100

    Delete of duplicated outputs

--- terraform/main.tf
+++ terraform/main.tf
@@ -32,10 +32,10 @@ module "ecs" {
 
   # wordpress_subnet_ids       = module.networking.wordpress_subnet_ids
   # Test
-  wordpress_subnet_ids       = module.networking.publics_subnet_ids
-  security_group_id          = module.security_group.ecs_security_group_id
-  efs_id                     = module.efs.efs_id
-  wordpress_image            = var.wordpress_image
+  wordpress_subnet_ids = module.networking.publics_subnet_ids
+  security_group_id    = module.security_group.ecs_security_group_id
+  efs_id               = module.efs.efs_id
+  wordpress_image      = var.wordpress_image
   rds_database = {
     db_address  = module.rds.db_address
     db_name     = var.db_name
--- terraform/modules/efs/output.tf
+++
@@ -1,7 +0,0 @@
-output "efs_id" {
-  value = module.efs.id
-}
-
-output "efs_security_group_id" {
-  value = module.efs.security_group_id
-}
